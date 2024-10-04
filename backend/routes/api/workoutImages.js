c8fa80ce739bc2e3117ce9f9aad46c415b3466d6
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-04 06:57:14 -0400

    ADD: WorkoutImages route now has full Crud

--- backend/routes/api/workoutImages.js
+++ backend/routes/api/workoutImages.js
@@ -1,17 +1,153 @@
-// backend/routes/api/workout.js
+// backend/routes/api/WorkoutImages.js
 
 const express = require('express');
 const { requireAuth } = require('../../utils/auth');
-const { properUserValidation } = require('../../utils/validation');
+const { properUserValidation, handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
+const { Op } = require('sequelize')
+const { User, DayLog, Grub, GrubIcon, GrubImage, Workout, WorkoutIcon, WorkoutImage } = require('../../db/models');
 
 
 const router = express.Router();
 
 
 router.get('/hello/world', (req, res) => {
-    console.log("=== HERE I AM 3 ===")
-    res.send('api/routes/workouts ---> Hello World!');
+    res.send('api/routes/food ---> Hello World!');
 });
+
+//all
+router.get('/', async (req, res, next) => {
+    
+    try {
+        const allGrubs = await WorkoutImage.findAll()
+        
+        
+        const answer = allGrubs.map(e=>{
+            const workoutJSON = e.toJSON();
+            console.log('\n--> e = ', workoutJSON)
+            return workoutJSON
+        })
+        // res.send('entered TRY-Block')       
+        res.json({WorkoutImages: answer})
+
+
+    } catch (e) {
+        // console.log('Route Error: ', e)
+        next(e)
+    }
+});
+
+//one
+router.get('/:grubId', async (req, res, next) => {
+    
+    try {
+
+        const grubId = parseInt(req.params.grubId)
+        const currentGrub = await WorkoutImage.findByPk(grubId, {
+            include: [{model: Workout}]
+        })
+        
+        
+        const currentGrubJSON = currentGrub.toJSON()
+
+        return res.status(201).json(currentGrubJSON)
+
+
+    } catch (e) {
+        // console.log('Route Error: ', e)
+        next(e)
+    }
+});
+
+
+router.post('/', async (req, res, next) => {
+    try {
+
+        const {url, workoutId, name} = req.body
+
+        const newGrub = await WorkoutImage.create(
+            {
+                url,
+                name,
+                workoutId: parseInt(workoutId)
+            }
+        )
+
+        let newWorkoutJSON = newGrub.toJSON();
+
+        let responseBody = {...newWorkoutJSON}
+        responseBody.createdAt = newWorkoutJSON.createdAt
+        responseBody.updatedAt = newWorkoutJSON.updatedAt
+        return res.status(201).json(responseBody)
+    } catch (e){
+        console.log('Route Error: ', e)
+        next(e)
+    }
+})
+
+router.delete('/:grubId', async (req, res, next) => {
+    try {
+        const grubId = parseInt(req.params.grubId)
+        const currentGrub = await WorkoutImage.findByPk(grubId)
+
+        if (!currentGrub){
+            res.status(404).json({
+                message: "Grub couldn't be found"
+            })
+        }
+
+        currentGrub.destroy();
+        res.json({"message": "Successfully deleted"})
+    } catch (e){
+        console.log('Route Error: ', e)
+        next(e)
+    }
+})
+
+
+router.put('/:WorkoutImageId', async (req, res, next) => {
+    // res.send('HELLO FROM put')
+
+    try {
+
+        console.log("===> A")
+        const WorkoutImageId = parseInt(req.params.WorkoutImageId)
+        const currentWorkoutImage = await WorkoutImage.findByPk(WorkoutImageId)
+
+        if (!currentWorkoutImage){
+            res.status(404).json({
+                message: "WorkoutImage couldn't be found"
+            })
+        }
+
+        console.log("===> B")
+
+        const {url, grubId, name} = req.body
+
+
+        await currentWorkoutImage.update(
+            {
+                name,
+                url,
+                grubId: parseInt(grubId)
+            }
+        )
+
+        console.log("===> C")
+        let currentWorkoutImageJSON = currentWorkoutImage.toJSON();
+
+
+        console.log("===> D")
+
+        // response.json(res)
+        return res.status(201).json(currentWorkoutImageJSON)
+        console.log("===> E")
+ 
+    } catch (e){
+        console.log('Route Error: ', e)
+        next(e)
+    }
+})
 
 
 module.exports = router;
