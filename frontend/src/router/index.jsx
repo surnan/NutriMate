4fb8304141908abf08b02a450fc09dd1847a5015
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-05 18:21:49 -0400

    CREATE: Redux components/routes for: food, workouts, weight.  This is likely going to be scrapped later.  These pages are to display redux store tests

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -4,6 +4,13 @@ import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
 import Splash from '../components/Splash';
 
+import WorkoutPage_ from '../components/WorkoutPage_'
+import WeightPage_ from '../components/WeightPage_'
+import FoodPage_ from '../components/FoodPage_'
+
+
+
+
 export const router = createBrowserRouter([
   {
     element: <Layout />,
@@ -11,6 +18,12 @@ export const router = createBrowserRouter([
       {path: "/", element: <Splash />},
       {path: "login", element: <LoginFormPage />},
       {path: "signup", element: <SignupFormPage />},
+      {path: "workouts", element: <WorkoutPage_ />},
+      {path: "weights", element: <WeightPage_ />},
+      {path: "food", element: <FoodPage_ />},
+      
+      {path: "*", element: <p>--Page Not Found--</p>}
+
     ],
   },
 
