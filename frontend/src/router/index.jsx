16abd3d9bd1740ad1bab6b8e776329ff3453a706
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-05 18:00:20 -0400

    CREATE: FrontEnd Skeleton for Redux Componenets: FoodPage_, WeightPage_, WorkPage_

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -8,18 +8,9 @@ export const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      {
-        path: "/",
-        element: <Splash />,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />,
-      },
+      {path: "/", element: <Splash />},
+      {path: "login", element: <LoginFormPage />},
+      {path: "signup", element: <SignupFormPage />},
     ],
   },
 
