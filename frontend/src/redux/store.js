d2496c6ed7d28668501ef59c0e674af402a01187
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-06 08:00:59 -0400

    ADD: GetAllWeightsThunk & output returned with api/weights.  Also, removed all redux and linter warnings

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import weightsReducer from "./weight";
 
 const rootReducer = combineReducers({
     session: sessionReducer,
+    weights: weightsReducer
 });
 
 let enhancer;
