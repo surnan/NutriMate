3505ac25af3eda931698ec6f8c926fc66a6e78bc
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-06 08:00:59 -0400

    ADD: GetAllWeightsThunk & output returned with api/weights.  Also, removed all redux and linter warnings

---
+++ frontend/src/components/WeightCard_/WeightCard.jsx
@@ -0,0 +1,18 @@
+// frontend/src/componenets/WeightPage_/WeightPage.jsx
+// import "./WeightCard.css";
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Navigate, useNavigate } from "react-router-dom";
+// import { getWeightsAllThunk } from "../../redux/weight";
+
+
+function WeightCard() {
+
+  return (
+    <>
+      <h1>!!  WeightCard.jsx  !!</h1>
+    </>
+  );
+}
+
+export default WeightCard;
