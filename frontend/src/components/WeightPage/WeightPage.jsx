b8c29f7e834b0e72f59c6c8041e8e64c71cfcfe9
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-06 09:31:53 -0400

    Create: <WeightPage> route that only returns <h1>

---
+++ frontend/src/components/WeightPage/WeightPage.jsx
@@ -0,0 +1,11 @@
+// frontend/src/components/WeightPage/WeightPage.jsx
+
+
+
+const WeightPage = () => {
+  return (
+        <h1>!! Weight Page !!</h1>
+  );
+}
+
+export default WeightPage;
--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -3,23 +3,17 @@ import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
 import Splash from '../components/Splash';
+import WeightPage from '../components/WeightPage';
 
 export const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      {
-        path: "/",
-        element: <Splash />,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />,
-      },
+      {path: "/", element: <Splash />},
+      {path: "login", element: <LoginFormPage />},
+      {path: "signup", element: <SignupFormPage />},
+      {path: "weights", element: <WeightPage />},
+      {path: "*" , element: <p> == Page NOT Found ==</p>},
     ],
   },
 
