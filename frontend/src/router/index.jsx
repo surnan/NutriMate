b677dd910cd6ce946182c11cb84e125dce9bf57f
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-07 17:35:58 -0400

    Workout Page succesfully showing all Workouts from Thunk

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -4,6 +4,7 @@ import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
 import Splash from '../components/Splash';
 import WeightPage from '../components/WeightPage'
+import WorkoutPage from '../components/WorkoutPage'
 import WeightPageForm from '../components/WeightPageForm/WeightPageForm';
 
 
@@ -16,6 +17,7 @@ export const router = createBrowserRouter([
       {path: "signup", element: <SignupFormPage />},
       {path: "weights", element: <WeightPage />},
       {path: "weightform", element: <WeightPageForm />},
+      {path: "workouts", element: <WorkoutPage />},
       {path: "*" , element: <p> == Page NOT Found ==</p>},
     ],
   },
