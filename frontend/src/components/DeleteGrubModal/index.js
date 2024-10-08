75e849493b7bfbc86d009896a9e7e8ab85842525
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-08 09:17:39 -0400

    ADD: GrubForm now creating new Grub and updating UI via Redux

--- frontend/src/components/DeleteGrubModal/index.js
+++ frontend/src/components/DeleteGrubModal/index.js
@@ -1,4 +1,4 @@
 // frontend/src/components/WeightCard/index.js
-import DeleteWorkoutModal from './DeleteWorkoutModal';
+import DeleteGrubModal from './DeleteGrubModal';
 
-export default DeleteWorkoutModal;
+export default DeleteGrubModal;
