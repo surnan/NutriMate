de839b0c3c7aa9bd9f965b40f82a77592a07c8f2
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-08 06:33:11 -0400

    CREATE: <GrubPage /> and added to redux routing table

---
+++ frontend/src/components/GrubPage/GrubPage.jsx
@@ -0,0 +1,23 @@
+// frontend/src/components/GrubPage/GrubPage.jsx
+
+import "./GrubPage.css";
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getWeightsAllThunk } from "../../redux/weight";
+// import { useNavigate } from "react-router-dom"
+// import WeightCard from "../WeightCard";
+// import DeleteWeightModal from '../DeleteWeightModal'
+
+
+const GrubPage = () => {
+
+
+
+
+  return (
+    <h1> GrubPage.jsx </h1>
+
+  );
+}
+
+export default GrubPage;
