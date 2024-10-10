8269a2f534480c30a57d6f4c1a70138c8252c52f
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-10 19:31:07 -0400

    WeightCard.jsx & WeightCard.css now have same format as WorkoutPageForm.jsx & WorkoutPageForm.css

--- frontend/src/components/WeightCard/WeightCard.jsx
+++ frontend/src/components/WeightCard/WeightCard.jsx
@@ -3,13 +3,22 @@ import "./WeightCard.css";
 
 function WeightCard({ weight }) {
 
-  const { current, start, goal } = weight
+  const { current, start, goal, day, User } = weight
+
+  const formattedDate = new Date(day).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 
   return (
-    <div className="weight_card_hFlex">
-      <p>Start Wt: {start}</p>
-      <p>Current Wt: {current}</p>
-      <p>Goal Wt: {goal}</p>
+    <div className="weight_card_grid">
+      <p><strong>Username:</strong> {User.username}</p>
+      <br/>
+      <p><strong>Date:</strong> {formattedDate}</p>
+      <p><strong>Start Wt:</strong> {start}</p>
+      <p><strong>Current Wt:</strong> {current}</p>
+      <p><strong>Goal Wt:</strong> {goal}</p>
     </div>
   );
 }
