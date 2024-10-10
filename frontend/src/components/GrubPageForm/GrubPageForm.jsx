cd6b1562e8d9791d726bc00a4a8ce28dc77ab15f
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-10 13:27:12 -0400

    GrubPageForm.jsx & GrubPageForm.css now have same format as WorkoutPageForm.jsx & WorkoutPageForm.css

--- frontend/src/components/GrubPageForm/GrubPageForm.jsx
+++ frontend/src/components/GrubPageForm/GrubPageForm.jsx
@@ -8,26 +8,16 @@ import DeleteGrubModal from '../DeleteGrubModal'
 
 
 function GrubPageForm() {
-    const nav = useNavigate();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-
     const location = useLocation();
     const { newGrub, exampleData } = location.state || {};
 
     const [showDeletetModal, setShowDeletetModal] = useState(false);
     const [selectedGrub, setSelectedGrub] = useState(null);
-
-    const handleDeleteBtn = (e) => {
-        e.preventDefault();
-        setSelectedGrub(exampleData);
-        setShowDeletetModal(true)
-    }
-
-    const handleModalClose = () => {
-        setShowDeletetModal(false)
-        setSelectedGrub(null)
-        nav(-1)
-    };
+    const [isEditing, setIsEditing] = useState(!exampleData);
+    // const [clickedSubmitBtn, setClickedSubmitBtn] = useState(false);
+    const [errors, setErrors] = useState({});
 
     const [form, setForm] = useState({
         name: exampleData?.name || "",
@@ -43,18 +33,31 @@ function GrubPageForm() {
         userId: exampleData?.userId || 1
     });
 
-    const [errors, setErrors] = useState({})
-    const [clickedSubmitBtn, setClickedSubmitBtn] = useState(false);
-    const hasError = () => (Object.keys(errors).length !== 0)
+    const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+    const hasError = () => Object.keys(errors).length !== 0;
 
-    const handleCancel = (e) => {
-        e.preventDefault();
-        nav(-1);  
+    const handleDeleteBtn = (e) => {
+        setSelectedGrub(exampleData);
+        setShowDeletetModal(true)
+    }
+
+    const handleModalClose = () => {
+        setShowDeletetModal(false)
+        setSelectedGrub(null)
+        navigate(-1)
+    };
+
+    const handleBackBtn = () => { 
+        console.log("==> handleBackBtn")
+        navigate(-1) 
     };
 
     const handleSubmit = async (e) => {
+        if (hasError()) {
+            return
+        }
         e.preventDefault();
-        setClickedSubmitBtn(true);
+        setIsEditing(false)
 
         const { name, servingUnit, servingSize, calories, protein, fats } = form
         const { carbs, sugar, company, description } = form
@@ -73,40 +76,52 @@ function GrubPageForm() {
             description,
             userId: 1,
         }
-        const submit = async () => {
-            try {
-                const result = newGrub
-                    ? await dispatch(updateGrubsOneThunk({ body }))
-                    : await dispatch(postGrubsOneThunk({ body }))
 
-                if (result) {
-                    nav(`/grubs`);
-                }
-            } catch (error) {
-                console.error('Error adding grub:', error);
-            }
+        try {
+            const result = newGrub
+                ? await dispatch(updateGrubsOneThunk({ body }))
+                : await dispatch(postGrubsOneThunk({ body }))
+            if (result) { navigate(`/grubs`) }
+        } catch (error) {
+            console.error('Error adding grub:', error);
         }
-        submit();
     }
 
-    const capitalizeFirstLetter = (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+    const handleCancelBtn = (e) => {
+        setIsEditing(false)
+        setForm({
+            name: exampleData?.name || "",
+            description: exampleData?.description,
+            servingUnit: exampleData?.servingUnit,
+            servingSize: exampleData?.servingSize,
+            calories: exampleData?.calories,
+            protein: exampleData?.protein,
+            fats: exampleData?.fats,
+            carbs: exampleData?.carbs,
+            sugar: exampleData?.sugar,
+            company: exampleData?.company,
+            description: exampleData?.description,
+            userId: exampleData?.userId || 1
+
+
+        });
+    };
+
+    const handleUpdateBtn = () => {
+        setIsEditing(true);
     };
 
     useEffect(() => {
         const newErrors = {};
         const allKeys = ["name", "servingUnit", "servingSize", "calories", "protein", "fats", "carbs", "sugar", "company", "description"];
 
-        for (let key of allKeys) {
+        allKeys.forEach((key) => {
             if (!form[key]) {
-                newErrors[key] = capitalizeFirstLetter(`${key} is required`);
+                newErrors[key] = `${capitalizeFirstLetter(key)} is required`;
             }
-        }
-
-        if (clickedSubmitBtn) {
-            setErrors(newErrors)
-        }
-    }, [form, clickedSubmitBtn, showDeletetModal])
+        });
+        setErrors(newErrors);
+    }, [form])
 
 
     const updateSetForm = (e) => {
@@ -117,153 +132,170 @@ function GrubPageForm() {
     return (
         <div>
             <h1>GrubForm.jsx</h1>
-            <label>
-                Name &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.name}</span>}
-            </label>
-            <input
-                type="text"
-                name="name"
-                onChange={updateSetForm}
-                placeholder="enter name"
-                value={form.name || ""}
-            />
 
-            <br />
-            <label>
-                Serving Unit &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.servingUnit}</span>}
-            </label>
-            <input
-                type="text"
-                name="servingUnit"
-                onChange={updateSetForm}
-                placeholder="enter serving Unit"
-                value={form.servingUnit || ""}
-            />
+            <div className="grubPageForm_hFlex">
+                <button
+                    className="back_btn"
+                    type="button"
+                    onClick={handleBackBtn}
+                >
+                    BACK
+                </button>
 
-            <br />
-            <label>
-                Serving Size &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.servingSize}</span>}
-            </label>
-            <input
-                type="text"
-                name="servingSize"
-                onChange={updateSetForm}
-                placeholder="enter serving size"
-                value={form.servingSize || ""}
-            />
+                {isEditing ? (
+                    <button
+                        className="back_btn green"
+                        type="button"
+                        onClick={handleSubmit}
+                        disabled={hasError()}
+                    >
+                        SAVE
+                    </button>
+                ) : (
+                    <button
+                        className="back_btn blue"
+                        type="button"
+                        onClick={handleUpdateBtn}>
+                        UPDATE
+                    </button>
+                )}
+            </div>
 
 
-            <br />
-            <label>
-                Calories &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.calories}</span>}
-            </label>
-            <input
-                type="text"
-                name="calories"
-                onChange={updateSetForm}
-                placeholder="enter calories"
-                value={form.calories || ""}
-            />
+            <div className="grub_page_form_grid">
 
-            <br />
-            <label>
-                Protein &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.protein}</span>}
-            </label>
-            <input
-                type="text"
-                name="protein"
-                onChange={updateSetForm}
-                placeholder="enter protein"
-                value={form.protein || ""}
-            />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.name && <span style={{ color: 'red' }}>{errors.name}&nbsp;&nbsp;</span>} Name:
+                </label>
+                <input
+                    type="text"
+                    name="name"
+                    onChange={updateSetForm}
+                    placeholder="enter name"
+                    value={form.name || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <label>
-                Fats &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.fats}</span>}
-            </label>
-            <input
-                type="text"
-                name="fats"
-                onChange={updateSetForm}
-                placeholder="enter fats"
-                value={form.fats || ""}
-            />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.servingUnit && <span style={{ color: 'red' }}>{errors.servingUnit} &nbsp;&nbsp; </span>}Serving Unit
+                </label>
+                <input
+                    type="text"
+                    name="servingUnit"
+                    onChange={updateSetForm}
+                    placeholder="enter serving Unit"
+                    value={form.servingUnit || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <label>
-                Carbs &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.carbs}</span>}
-            </label>
-            <input
-                type="text"
-                name="carbs"
-                onChange={updateSetForm}
-                placeholder="enter carbs"
-                value={form.carbs || ""}
-            />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.servingSize && <span style={{ color: 'red' }}>{errors.servingSize}&nbsp;&nbsp;</span>} Serving Size
+                </label>
+                <input
+                    type="text"
+                    name="servingSize"
+                    onChange={updateSetForm}
+                    placeholder="enter serving size"
+                    value={form.servingSize || ""}
+                    readOnly={!isEditing}
+                />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.calories && <span style={{ color: 'red' }}>{errors.calories}&nbsp;&nbsp;</span>} Calories
+                </label>
+                <input
+                    type="text"
+                    name="calories"
+                    onChange={updateSetForm}
+                    placeholder="enter calories"
+                    value={form.calories || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <label>
-                Sugar &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.sugar}</span>}
-            </label>
-            <input
-                type="text"
-                name="sugar"
-                onChange={updateSetForm}
-                placeholder="enter sugar"
-                value={form.sugar || ""}
-            />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.protein && <span style={{ color: 'red' }}>{errors.protein}&nbsp;&nbsp;</span>} Protein
+                </label>
+                <input
+                    type="text"
+                    name="protein"
+                    onChange={updateSetForm}
+                    placeholder="enter protein"
+                    value={form.protein || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <label>
-                Company &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.company}</span>}
-            </label>
-            <input
-                type="text"
-                name="company"
-                onChange={updateSetForm}
-                placeholder="enter company name"
-                value={form.company || ""}
-            />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.fats && <span style={{ color: 'red' }}>{errors.fats}&nbsp;&nbsp;</span>} Fats
+                </label>
+                <input
+                    type="text"
+                    name="fats"
+                    onChange={updateSetForm}
+                    placeholder="enter fats"
+                    value={form.fats || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <label>
-                Description &#160;&#160;{errors.start && <span style={{ color: 'red' }}>{errors.description}</span>}
-            </label>
-            <input
-                type="text"
-                name="description"
-                onChange={updateSetForm}
-                placeholder="enter description"
-                value={form.description || ""}
-            />
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.carbs && <span style={{ color: 'red' }}>{errors.carbs}</span>} Carbs
+                </label>
+                <input
+                    type="text"
+                    name="carbs"
+                    onChange={updateSetForm}
+                    placeholder="enter carbs"
+                    value={form.carbs || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <button
-                type="submit"
-                // disabled={hasError()}
-                onClick={handleSubmit}
-                className={`formBtn submitButton ${!hasError() ? 'enabledButton' : ''}`}
-            >
-                Create Grub
-            </button>
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.sugar && <span style={{ color: 'red' }}>{errors.sugar}&nbsp;&nbsp;</span>}Sugar
+                </label>
+                <input
+                    type="text"
+                    name="sugar"
+                    onChange={updateSetForm}
+                    placeholder="enter sugar"
+                    value={form.sugar || ""}
+                    readOnly={!isEditing}
+                />
 
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.company && <span style={{ color: 'red' }}>{errors.company}&nbsp;&nbsp;</span>} Company
+                </label>
+                <input
+                    type="text"
+                    name="company"
+                    onChange={updateSetForm}
+                    placeholder="enter company name"
+                    value={form.company || ""}
+                    readOnly={!isEditing}
+                />
 
-            <br />
-            <button
-                type="cancel"
-                onClick={handleCancel}
-                className="formBtn"
-            >
-                Cancel
-            </button>
-            <br />
-            <button
-                type="cancel"
-                onClick={handleDeleteBtn}
-                className="formBtn"
-            >
-                DELETE
-            </button>
+                <label style={{ display: 'inline-flex' }}>
+                    {errors.description && <span style={{ color: 'red' }}>{errors.description}&nbsp;&nbsp;</span>} Description
+                </label>
+                <input
+                    type="text"
+                    name="description"
+                    onChange={updateSetForm}
+                    placeholder="enter description"
+                    value={form.description || ""}
+                    readOnly={!isEditing}
+                />
+            </div>
+
+            <div className="workout_page_btn_grid">
+                {exampleData && (
+                    <button
+                        className="back_btn red"
+                        type="button"
+                        onClick={isEditing ? handleCancelBtn : handleDeleteBtn}
+                    >
+                        {isEditing ? "CANCEL" : "DELETE"}
+                    </button>
+                )}
+            </div>
+
 
 
             {showDeletetModal && (
@@ -281,3 +313,33 @@ function GrubPageForm() {
 }
 
 export default GrubPageForm;
+
+
+
+            
+{/* <button
+type="submit"
+// disabled={hasError()}
+onClick={handleSubmit}
+className={`formBtn submitButton ${!hasError() ? 'enabledButton' : ''}`}
+>
+Create Grub
+</button>
+
+
+<br />
+<button
+type="cancel"
+onClick={handleCancel}
+className="formBtn"
+>
+Cancel
+</button>
+<br />
+<button
+type="cancel"
+onClick={handleDeleteBtn}
+className="formBtn"
+>
+DELETE
+</button> */}
