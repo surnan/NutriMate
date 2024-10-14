8118db2b7c0a332b9564d7d27a032c9521dd10fc
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-14 04:34:09 -0400

    MINOR CSS Updates.  MVP check necessary

--- frontend/src/components/Splash/Splash.jsx
+++ frontend/src/components/Splash/Splash.jsx
@@ -23,8 +23,8 @@ const Splash = () => {
     <div className="mainBodyStyle">
       {sessionUser ? (
         <>
-          <h3 className="splash_showEmail">Email = {sessionUser?.email}</h3>
-          <div className="splash_grid">
+          {/* <h3 className="splash_showEmail">Email = {sessionUser?.email}</h3> */}
+          <div className="splash_grid twenty_padding">
             <button className="splashButton green shadow" onClick={handleWeightsBtn}>weights</button>
             <button className="splashButton orange shadow" onClick={handleWorkoutsBtn}>workouts</button>
             <button className="splashButton blue shadow" onClick={handleGrubsBtn}>grubs</button>
