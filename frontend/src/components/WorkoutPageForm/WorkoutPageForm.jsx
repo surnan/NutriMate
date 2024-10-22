cf5154d842f304694791bae9adcb212307e7563b
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-21 20:35:22 -0400

    Code Cleanup: workoutPage.jsx + WorkoutPageForm

--- frontend/src/components/WorkoutPageForm/WorkoutPageForm.jsx
+++ frontend/src/components/WorkoutPageForm/WorkoutPageForm.jsx
@@ -11,13 +11,13 @@ function WorkoutPageForm() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const location = useLocation();
-    const sessionUser = useSelector((state) => state.session.user);
     const { newWorkout, currentData } = location.state || {};
-
+    const sessionUser = useSelector((state) => state.session.user);
     const [showDeletetModal, setShowDeletetModal] = useState(false);
     const [selectedWorkout, setSelectedWorkout] = useState(null);
     const [errors, setErrors] = useState({});
-
+    const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+    const hasError = () => Object.keys(errors).length !== 0;
 
     const [form, setForm] = useState({
         name: currentData?.name || "",
@@ -25,44 +25,26 @@ function WorkoutPageForm() {
         userId: currentData?.userId || sessionUser?.id || 1
     });
 
-    const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-    const hasError = () => Object.keys(errors).length !== 0;
-
-    const handleDeleteBtn = () => {
-        if (!currentData?.id) {
-            alert('Can not delete this new record because it has not been saved to database');
-            return;
-        }
-        setSelectedWorkout(currentData);
-        setShowDeletetModal(true)
+    const updateSetForm = (e) => {
+        const { name, value } = e.target;
+        setForm(prev => ({ ...prev, [name]: value }))
     }
 
-    const handleDayLogBtn = () => {
-        if (!currentData?.id) {
-            alert('Workout needs to be saved before adding to DayLog');
-            return;
-        }
-        navigate('/DayLogFormWorkout')
+    const handleBack = () => navigate(-1);
+
+    const handleReset = () => {
+        setForm({
+            name: currentData?.name || "",
+            description: currentData?.description || ""
+        });
     }
 
-
-
-
-    const handleModalClose = () => {
-        setShowDeletetModal(false)
-        setSelectedWorkout(null)
-        navigate(-1)
-    };
-
-    const handleBackBtn = () => { navigate(-1) };
-
-    const handleSubmit = async (e) => {
+    const handleSubmitSave = async (e) => {
         if (hasError()) {
             return
         }
 
         e.preventDefault();
-
         const { name, description, userId } = form;
         const body = {
             id: parseInt(currentData?.id),
@@ -70,9 +52,6 @@ function WorkoutPageForm() {
             description,
             userId
         }
-
-        console.log("===> line 56 ===> body = ", body)
-
         try {
             const result = newWorkout
                 ? await dispatch(updateWorkoutsOneThunk({ body }))
@@ -83,13 +62,29 @@ function WorkoutPageForm() {
         }
     }
 
-    const handleresetBtn = () => {
-        setForm({
-            name: currentData?.name || "",
-            description: currentData?.description || ""
-        });
+
+    const handleDelete = () => {
+        if (!currentData?.id) {
+            alert('Can not delete this new record because it has not been saved to database');
+            return;
+        }
+        setSelectedWorkout(currentData);
+        setShowDeletetModal(true)
     }
 
+    const handleAddToLog = () => {
+        if (!currentData?.id) {
+            alert('Workout needs to be saved before adding to DayLog');
+            return;
+        }
+        navigate('/DayLogFormWorkout', { state: { newWorkout: true, currentData: workout } });
+    }
+
+    const handleModalClose = () => {
+        setShowDeletetModal(false)
+        setSelectedWorkout(null)
+        navigate(-1)
+    };
 
     useEffect(() => {
         const newErrors = {};
@@ -103,28 +98,17 @@ function WorkoutPageForm() {
         setErrors(newErrors);
     }, [form])
 
-
-    const updateSetForm = (e) => {
-        const { name, value } = e.target;
-        setForm(prev => ({ ...prev, [name]: value }))
-    }
-
-    const formatDate = (dateString) => {
-        if (!dateString) return new Date().toISOString().split('T')[0]; // Use current date if not provided
-        const date = new Date(dateString);
-        return !isNaN(date.getTime()) ? date.toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
-    };
-
     return (
         <div className="mainBodyStyle">
-            {/* <h1>WorkoutPageForm.jsx</h1>
-            <h3 >Email = {sessionUser?.email}</h3> */}
+            <h1>WorkoutPageForm.jsx</h1>
+            <h3 >Email = {sessionUser?.email}</h3>
 
+            {/* TOP BUTTONS */}
             <div className="max_HFlex">
                 <button
                     className="blue _button"
                     type="button"
-                    onClick={handleBackBtn}
+                    onClick={handleBack}
                 >
                     BACK
                 </button>
@@ -133,14 +117,14 @@ function WorkoutPageForm() {
                     <button
                         className="orange _button"
                         type="button"
-                        onClick={handleresetBtn}
+                        onClick={handleReset}
                     >
                         RESET
                     </button>
                     <button
                         className="green _button"
                         type="button"
-                        onClick={handleSubmit}
+                        onClick={handleSubmitSave}
                         disabled={hasError()}
                     >
                         SAVE
@@ -150,9 +134,13 @@ function WorkoutPageForm() {
             </div>
 
 
+            {/*  INPUT FIELDS */}
             <div className="workout_page_form_grid">
                 <label style={{ display: 'inline-flex' }}>
-                    {errors.name && <span style={{ color: 'red' }}>{errors.name}&nbsp;&nbsp;</span>} Name:
+                    {errors.name &&
+                        <span style={{ color: 'red' }}>{errors.name}&nbsp;&nbsp;</span>
+                    }
+                    Name:
                 </label>
 
                 <input
@@ -165,7 +153,9 @@ function WorkoutPageForm() {
                 />
 
                 <label style={{ display: 'inline-flex' }}>
-                    {errors.description && <span style={{ color: 'red' }}>{errors.description}&nbsp;&nbsp;</span>} Description:
+                    {errors.description &&
+                        <span style={{ color: 'red' }}>{errors.description}&nbsp;&nbsp;</span>}
+                    Description:
                 </label>
                 <textarea
                     className="_textarea"
@@ -175,84 +165,35 @@ function WorkoutPageForm() {
                     placeholder="Enter description"
                     value={form.description}
                 />
-
             </div>
 
-            <button
-                className="red _button"
-                type="button"
-                onClick={handleDeleteBtn}
-            >
-                DELETE
-            </button>
+            {/* BOTTOM BUTTONS */}
+            <div className="max_HFlex">
+                <button
+                    className="red _button"
+                    type="button"
+                    onClick={handleDelete}
+                >
+                    DELETE
+                </button>
 
-            <button
-                className="black _button"
-                type="button"
-            onClick={handleDayLogBtn}
-            >
-                Add To Log
-            </button>
-
+                <button
+                    className="black _button"
+                    type="button"
+                    onClick={handleAddToLog}
+                >
+                    Add To Log
+                </button>
+            </div>
 
             {showDeletetModal && (
                 <DeleteWorkoutModal
                     onClose={handleModalClose}
-                    onSubmit={handleDeleteBtn}
+                    onSubmit={handleDelete}
                     workout={selectedWorkout}
                 />
             )}
-            <br />
-            <br />
-            <hr />
-            <br />
-            <br />
-
-
-            <div className="workout_page_form_grid">
-
-                <p>Date</p>
-                <input
-                    className="_input"
-                    type="datetime-local"
-                    name="day"
-                    onChange={updateSetForm}
-                    placeholder="Please enter your goal weight"
-                    value={formatDate(form.day)}
-                />
-                <p>Calories</p>
-                <input
-                    className="_input"
-                    type="number"
-                    name="day"
-                    onChange={updateSetForm}
-                    placeholder="Please enter your goal weight"
-                    value={formatDate(form.day)}
-                />
-                <input
-                    className="_input"
-                    type="number"
-                    name="Quantity"
-                    onChange={updateSetForm}
-                    placeholder="Quantity"
-                    value={formatDate(form.day)}
-                />
-                <select
-                    className="_input"
-                    name="servingUnit"
-                    onChange={updateSetForm}
-                    value={form.servingUnit || ""}
-                >
-                    <option value="">Quantity Type</option>
-                    <option value="hours">hours</option>
-                    <option value="minutes">minutes</option>
-                    <option value="seconds">seconds</option>
-                    <option value="each">each</option>
-                    <option value="reps">reps</option>
-                </select>
-
-            </div>
-        </div>
+        </div >
     );
 }
 export default WorkoutPageForm;
