5fa2d344cb6daae4d8e88a956b3656d3a0452725
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-23 07:17:45 -0400

    REDO: GrubPageForm to similar format as WorkoutPageForm

--- frontend/src/utils/MyFunctions.js
+++ frontend/src/utils/MyFunctions.js
@@ -1,2 +1,9 @@
 export const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
+export const formatDate = (dateString) => {
+    if (!dateString) return new Date().toISOString().split('T')[0]; // Use current date if not provided
+    const date = new Date(dateString);
+    return !isNaN(date.getTime()) ? date.toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
+};
+
+export const isEmpty = (obj) => Object.keys(obj).length !== 0;
