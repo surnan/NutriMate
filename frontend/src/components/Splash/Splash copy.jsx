862b76be066a02f89f55f6a86ff481acea9b9390
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-28 07:57:10 -0400

    ADD+BUG: aws for profile pic & displays next to login/logout profile button. However, it only shows after refresh on splash page.  Not on initial login.  Adding user to navigation component useeffect causes infinit loop calling "GET /api/restoreuser"

---
+++ frontend/src/components/Splash/Splash copy.jsx
@@ -0,0 +1,46 @@
+// frontend/src/components/Splash/Splash.jsx
+
+import "./Splash.css"
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Splash = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const nav = useNavigate()
+
+  const handleWeightsBtn = () => { nav("/weights") }
+  const handleWorkoutsBtn = () => { nav("/workouts") }
+  const handleGrubsBtn = () => { nav("/grubs") }
+  const handleDailyBtn = () => { nav("/daylog") }
+  const handleDayWorkoutBtn = () => { nav("/DayLogFormWorkout") }
+  const handleDayGrubBtn = () => { nav("/DayLogFormGrub") }
+
+  useEffect(() => {
+    if (sessionUser) {
+      console.log("Session user loaded:", sessionUser);
+    }
+  }, [sessionUser]);
+
+  return (
+    <div className="mainBodyStyle">
+      {sessionUser ? (
+        <>
+          <h3 className="splash_showEmail">Email = {sessionUser?.email}</h3>
+          <div className="splash_grid twenty_padding">
+            <button className="splashButton green shadow" onClick={handleWeightsBtn}>weights</button>
+            <button className="splashButton orange shadow" onClick={handleWorkoutsBtn}>workouts</button>
+            <button className="splashButton blue shadow" onClick={handleGrubsBtn}>grubs</button>
+            <button className="splashButton pink shadow" onClick={handleDailyBtn}>daily</button>
+            <button className="splashButton black shadow" onClick={handleDayWorkoutBtn}>day workout</button>
+            <button className="splashButton green shadow" onClick={handleDayGrubBtn}>day grub</button>
+          </div>
+        </>
+      ) : (
+        <h3>Please login to access the app</h3>
+      )}
+    </div>
+  );
+};
+
+export default Splash;
