a554ab127612685170ed17aabcdc525f4d5e1004
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-10-30 06:29:49 -0400

    GrubPageForm using external component for GrubImage

--- frontend/src/components/GrubPageForm/GrubImageDisplay.jsx
+++ frontend/src/components/GrubPageForm/GrubImageDisplay.jsx
@@ -1,28 +1,65 @@
-// frontend/src/components/GrubImageDisplay/GrubImageDisplay.jsx
-import React from 'react';
+// frontend/src/components/GrubPageFormGrubImageDisplay.jsx
+import React, { useEffect, useState } from "react";
 
-function GrubImageDisplay({ grubImgArr, downloadGIF, placeholderIMG, handleImgClick, isImageLoading }) {
-    return (
-        <div className="grub-image-display">
-            {isImageLoading ? (
-                <img src={loadingGif} alt="Loading..." className="loading-animation" />
-            ) : (
-                grubImgArr && grubImgArr.length > 0 ? (
-                    grubImgArr.map((img, index) => (
-                        <img
-                            key={index}
-                            src={img.url || placeholderIMG}
-                            alt="Grub Image"
-                            onClick={() => handleImgClick(img.id)}
-                            className="grub-image"
-                        />
-                    ))
-                ) : (
-                    <img src={placeholderIMG} alt="Placeholder" className="grub-placeholder-image" />
-                )
-            )}
+function GrubImageDisplay({ grubImgArr, downloadGIF, placeholderIMG, handleImgClick }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (grubImgArr && grubImgArr.length > 0) {
+      let loadedImages = 0;
+      
+      // Preload each image and check when all are fully loaded
+      grubImgArr.forEach((image) => {
+        const img = new Image();
+        img.src = image.url;
+        img.onload = () => {
+          loadedImages += 1;
+          if (loadedImages === grubImgArr.length) {
+            setLoading(false); // Set loading to false only when all images are fully loaded
+          }
+        };
+      });
+    } else {
+      setLoading(false); // Stop loading if there are no images
+    }
+  }, [grubImgArr]);
+
+  return (
+    <div>
+      {loading ? (
+        <div className="loadingGifDiv">
+          <img src={downloadGIF} alt="Loading..." className="downloadGIF" />
         </div>
-    );
+      ) : (
+        <div>
+          {grubImgArr && grubImgArr.length > 0 ? (
+            grubImgArr.map((currentImg) => (
+              <div key={currentImg.id}>
+                <img
+                  src={currentImg.url}
+                  style={{ height: "300px", width: "300px" }}
+                  alt="Grub Image"
+                  onClick={() => handleImgClick(currentImg.id)}
+                  className="clickable"
+                />
+              </div>
+            ))
+          ) : (
+            <div>
+              <br />
+              <img
+                src={placeholderIMG}
+                style={{ height: "300px", width: "300px" }}
+                alt="Grub Placeholder"
+                className="clickable"
+              />
+              <br />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default GrubImageDisplay;
