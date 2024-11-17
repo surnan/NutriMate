3f7181e69219860166fa806d53bf0acc40f2ff43
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-11-17 13:39:41 -0500

    All modals are dismissed when clicking outside modal div

--- frontend/src/components/_modal/DeleteModal/DeleteModal.jsx
+++ frontend/src/components/_modal/DeleteModal/DeleteModal.jsx
@@ -18,14 +18,15 @@ const DeleteModal = ({ item, itemType, deleteThunk, onClose }) => {
     const itemTypeText = itemType.charAt(0).toUpperCase() + itemType.slice(1); // Capitalize item type for display
 
     return (
-        <div className="modal_under_layer">
+        <>
+            <div className="modal_under_layer" onClick={onClose} /> 
             <div className="modal_window_all">
                 <h3>Confirm Delete</h3>
                 <p>Are you sure you want to delete this {itemType}?</p>
                 <button onClick={handleDelete} className="red _button">Yes (Delete {itemTypeText})</button>
                 <button onClick={onClose} className="green _button">No (Keep {itemTypeText})</button>
             </div>
-        </div>
+        </>
     );
 };
 
