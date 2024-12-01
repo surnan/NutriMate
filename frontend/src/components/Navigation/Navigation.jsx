94fd83bf77712be8328561a44de375d30ecb0cba
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-12-01 11:25:32 -0500

    darkmode toggle switch not affects DayLogPage.jsx. Next step is to push across all components & alter maring/padding to accomodate color themes

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -7,10 +7,18 @@ import logo from "../../fe_images/nutimate_logo.png";
 import { useEffect } from "react";
 import { thunkAuthenticate } from "../../redux/session";
 
+import { useTheme } from "../../context/ThemeContext"
+
 function Navigation() {
   const user = useSelector((state) => state.session.user);
   const navigate = useNavigate();
   const dispatch = useDispatch()
+  
+  const { theme, toggleTheme } = useTheme();
+
+  useEffect(() => {
+    console.log(`Navigation -> Theme ===> `, theme)
+}, [theme])
 
   useEffect(() => {
     dispatch(thunkAuthenticate());
@@ -18,7 +26,11 @@ function Navigation() {
   }, [dispatch, user?.email]);
 
   return (
-    <div className="nav_flex container-width navBlue">
+    <div 
+    // className="nav_flex container-width navBlue">
+    className={`nav_flex container-width navBlue
+      ${theme === "dark" ? "dkNav smoke_font" : ""}
+    `}>
       <NavLink to="/">
         <img
           src={logo}
